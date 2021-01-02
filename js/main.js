5bcf140e66731e0b287c705368f370539cae8411
Alen <alenjovancom@gmail.com>
2021-01-02 18:07:14 +0100

    prettify

--- js/main.js
+++ js/main.js
@@ -1,99 +1,92 @@
 "use strict";
 
-var globalUI = {
-  mainContainer: "gridMain",
-  gridSize: "gridSize",
-  btnGenerate: "btn-initialize",
-  btnReset: "btn-reset",
-  gridField: "gridField",
-  gridFieldContainer: "gridField__container",
-  colorPicker: "colorPick",
-  colorPickerCheckBox: "usePicker",
-  defaultColor: "rgb(255, 255, 255)",
-}; 
+  var globalUI = {
+    mainContainer: ".gridMain",
+    gridSize: "#gridSize",
+    btnGenerate: "#btn-initialize",
+    gridField: ".gridField",
+    gridFieldClass: "gridField",
+    gridFieldContainer: ".gridField__container",
+    gridFieldContainerClass: "gridField__container",
+    colorPicker: "#colorPick",
+    colorPickerCheckBox: "#usePicker",
+    defaultColor: "rgb(255, 255, 255)",
+    popupBtn: ".popup__btn-close",
+    popup: "#popup"
+  };
 
-//Main function
+  //setup button event handlers
 
-var initialize = function initialize() {
-  var gridSize = parseInt(
-    document.getElementById(globalUI.gridSize).value
-  );
-  var inputIsValid = checkUserInput(gridSize);
+  $(globalUI.btnGenerate).on("click", initialize);
 
-  if (inputIsValid) {
-    generateEmptyGrid(gridSize);
-    generateEventListeners();
+  $(globalUI.popupBtn).on("click", function () {
+    $(globalUI.popup).fadeOut(300);
+  });
+
+  //Main function
+
+  function initialize() {
+    var gridSize = parseInt($(globalUI.gridSize).val());
+    var inputIsValid = checkUserInput(gridSize);
+
+    if (inputIsValid) {
+      generateEmptyGrid(gridSize);
+      generateEventListeners();
+    }
   }
-}; 
 
-//Check if valid input
+  //Check if valid input
 
-var checkUserInput = function checkUserInput(number) {
-  if (number && number > 15 && number < 65) {
-    return true;
+  function checkUserInput(number) {
+    if (number && number > 15 && number < 65) {
+      return true;
+    }
+
+    $(globalUI.popup).fadeIn(300);
+    return false;
   }
-  $('#popup').fadeIn(300);
-  return false;
-}; 
 
-//Generate listeners for each grid field, define grid field behaviour
+  //Generate listeners for each grid field, define grid field behaviour
 
-var generateEventListeners = function generateEventListeners() {
-  $("." + globalUI.gridField).each(function (i) {
-    this.addEventListener(
-      "mouseenter",
-      function (event) {
-        var colorCheckBox = document.getElementById(
-          globalUI.colorPickerCheckBox
-        ).checked;
+  function generateEventListeners() {
+    $(globalUI.gridField).each(function (i) {
+      this.addEventListener("mouseenter", function (event) {
+        var colorCheckBox = $(globalUI.colorPickerCheckBox).prop("checked");
 
         if (
           colorCheckBox &&
           this.style.backgroundColor === globalUI.defaultColor
         ) {
-          this.style.backgroundColor = document.getElementById(
-            globalUI.colorPicker
-          ).value;
-        } else if (
-          this.style.backgroundColor === globalUI.defaultColor
-        ) {
+          this.style.backgroundColor = $(globalUI.colorPicker).val();
+        } else if (this.style.backgroundColor === globalUI.defaultColor) {
           this.style.backgroundColor =
             "#" + Math.floor(Math.random() * 16777215).toString(16);
         } else {
           this.style.backgroundColor = globalUI.defaultColor;
         }
-      }
-    );
-  });
+      });
+    });
+  }
 
-};
+  //Creates grid with default rgb color - white
 
-//Creates grid with default rgb color - white
+  function generateEmptyGrid(number) {
+    $(globalUI.gridFieldContainer).remove();
 
-var generateEmptyGrid = function generateEmptyGrid(number) {
-  $("." + globalUI.gridFieldContainer).remove();
-
-  for (var index = 0; index < number; index++) {
-    $("." + globalUI.mainContainer).append(
-      '<div class="' + globalUI.gridFieldContainer + '"></div>'
-    );
-
-    for (var _index = 0; _index < number; _index++) {
-      $("." + globalUI.gridFieldContainer + ":last-child").append(
-        '<div style="background-color: ' +
-        globalUI.defaultColor +
-        ';" class="' +
-        globalUI.gridField +
-        '"></div>'
+    for (var index = 0; index < number; index++) {
+      $(globalUI.mainContainer).append(
+        '<div class="' + globalUI.gridFieldContainerClass + '"></div>'
       );
+
+      for (var _index = 0; _index < number; _index++) {
+        $(globalUI.gridFieldContainer + ":last-child").append(
+          '<div style="background-color: ' +
+          globalUI.defaultColor +
+          ';" class="' +
+          globalUI.gridFieldClass +
+          '"></div>'
+        );
+      }
     }
   }
-};
 
-$('.popup__btn-close').on('click', function () {
-  $('#popup').fadeOut(300);
-});
-
-document
-  .getElementById(globalUI.btnGenerate)
-  .addEventListener("click", initialize);
